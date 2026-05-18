526d890efcadf6273a4b961126346b6554a683ae
Jit-codes-ez <jithazraedu@gmail.com@users.noreply.github.com>
2026-05-19 04:26:09 +0530

    Created Environment

--- api/GetImage.js
+++ api/GetImage.js
@@ -2,48 +2,29 @@ export default async function handler(req, res) {
 
   const path = req.query.path;
 
-  const token = process.env.GITHUB_TOKEN;
-
-  if (!token) {
-    return res.status(500).send("Token missing");
-  }
-
-  try {
-
-    const response = await fetch(
-      `https://api.github.com/repos/Jit-codes-ez/SathiSitePrivate/contents/${path}`,
-      {
-        headers: {
-          Authorization: `token ${token}`,
-          Accept: "application/vnd.github.v3.raw"
-        }
+  const response = await fetch(
+    `https://api.github.com/repos/Jit-codes-ez/SathiSitePrivate/contents/${path}`,
+    {
+      headers: {
+        Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
+        Accept: "application/vnd.github.v3.raw"
       }
-    );
-
-    if (!response.ok) {
-
-      const errorText = await response.text();
-
-      return res
-        .status(response.status)
-        .send(errorText);
     }
+  );
 
-    const arrayBuffer =
-      await response.arrayBuffer();
+  if (!response.ok) {
 
-    const buffer =
-      Buffer.from(arrayBuffer);
+    const error = await response.text();
 
-    res.setHeader(
-      "Content-Type",
-      response.headers.get("content-type")
-    );
-
-    return res.send(buffer);
-
-  } catch (err) {
-
-    return res.status(500).send(err.message);
+    return res.status(response.status).send(error);
   }
+
+  const buffer = Buffer.from(await response.arrayBuffer());
+
+  res.setHeader(
+    "Content-Type",
+    response.headers.get("content-type")
+  );
+
+  res.send(buffer);
 }
