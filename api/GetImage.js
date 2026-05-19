f1e756edee943648afa135db3870026a9b8c1bdf
Jit-codes-ez <jithazraedu@gmail.com@users.noreply.github.com>
2026-05-19 23:00:18 +0530

    Fixed dynamic image path

--- api/GetImage.js
+++ api/GetImage.js
@@ -1,12 +1,17 @@
 export default async function handler(req, res) {
+
   const owner = "Jit-codes-ez";
   const repo = "SathiSitePrivate";
-  const path = "Images/"; // file path inside repo
   const branch = "main";
 
-  const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branch}`;
+  // get image path from URL
+  const path = req.query.path;
+
+  const url =
+    `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branch}`;
 
   try {
+
     const response = await fetch(url, {
       headers: {
         Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
@@ -15,17 +20,24 @@ export default async function handler(req, res) {
     });
 
     if (!response.ok) {
-      return res.status(response.status).json({
-        error: "GitHub fetch failed"
-      });
+
+      const errorText = await response.text();
+
+      return res.status(response.status).send(errorText);
     }
 
-    const buffer = await response.arrayBuffer();
+    const buffer =
+      Buffer.from(await response.arrayBuffer());
 
-    res.setHeader("Content-Type", "image/jpeg");
-    res.send(Buffer.from(buffer));
+    res.setHeader(
+      "Content-Type",
+      response.headers.get("content-type")
+    );
+
+    res.send(buffer);
 
   } catch (err) {
+
     res.status(500).json({
       error: err.message
     });
