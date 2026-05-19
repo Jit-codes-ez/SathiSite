38c00688bca60d2a69ff85b8a3e4fa99fe1f8327
Jit-codes-ez <jithazraedu@gmail.com@users.noreply.github.com>
2026-05-19 22:46:49 +0530

    Code update

--- api/GetImage.js
+++ api/GetImage.js
@@ -1,30 +1,33 @@
 export default async function handler(req, res) {
+  const owner = "Jit-codes-ez";
+  const repo = "SathiSitePrivate";
+  const path = "Images/"; // file path inside repo
+  const branch = "main";
 
-  const path = req.query.path;
+  const url = `https://api.github.com/repos/${owner}/${repo}/contents/${path}?ref=${branch}`;
 
-  const response = await fetch(
-    `https://api.github.com/repos/Jit-codes-ez/SathiSitePrivate/${path}`,
-    {
+  try {
+    const response = await fetch(url, {
       headers: {
         Authorization: `Bearer ${process.env.GITHUB_TOKEN}`,
         Accept: "application/vnd.github.v3.raw"
       }
+    });
+
+    if (!response.ok) {
+      return res.status(response.status).json({
+        error: "GitHub fetch failed"
+      });
     }
-  );
 
-  if (!response.ok) {
+    const buffer = await response.arrayBuffer();
 
-    const error = await response.text();
+    res.setHeader("Content-Type", "image/jpeg");
+    res.send(Buffer.from(buffer));
 
-    return res.status(response.status).send(error);
+  } catch (err) {
+    res.status(500).json({
+      error: err.message
+    });
   }
-
-  const buffer = Buffer.from(await response.arrayBuffer());
-
-  res.setHeader(
-    "Content-Type",
-    response.headers.get("content-type")
-  );
-
-  res.send(buffer);
 }
